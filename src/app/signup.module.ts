358a3a5468efc7eaa9f73d39d01d16a5e5fad6b4
Afaq Ahmad <afaq11ahmad@gmaill.com>
2022-07-28 10:59:15 +0500

    added material ui and a signup page v0.1

---
+++ src/app/signup.module.ts
@@ -0,0 +1,26 @@
+import { SignupComponent } from './components/signup/signup.component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AngularMaterialModule } from './angular-material.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    FlexLayoutModule
+  ],
+  bootstrap: [SignupComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SignupModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { SignupModule } from './app/signup.module'
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(SignupModule)
   .catch(err => console.error(err));
